8a5dc8f1fa5e82282bdccefc6af7484ef0169527
Zixun Ai <aiz@uci.edu>
2020-01-01 17:24:37 -0800

    Update main.go

--- lambda-user-create/main.go
+++ lambda-user-create/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	encoder "expense-tracker/encoder"
+
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,23 +16,27 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+// Item - Database schema
 type Item struct {
 	Username     string    `json:"username"`
 	Password     string    `json:"password"`
 	CreationDate time.Time `json:"creation-time"`
 }
 
+// Request - Http request from the AWS API Gateway
 type Request struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
+// Response - Http response to the request from AWS API Gateway
 type Response struct {
-	Message string `json:"message`
+	Message string `json:"message"`
 	Key     string `json:"key"`
 	Ok      bool   `json:"ok"`
 }
 
+// DatabaseUserExist - Check if the user already exist in the database
 func DatabaseUserExist(item Item) bool {
 	// Load Credentials
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -66,6 +72,7 @@ func DatabaseUserExist(item Item) bool {
 	return false
 }
 
+// DatabaseCreateUser - Create a user item in the database
 func DatabaseCreateUser(item Item) {
 	// Load Credentials
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -99,6 +106,13 @@ func DatabaseCreateUser(item Item) {
 	fmt.Println("DynamoDB User Create Complete")
 }
 
+// Validate - Validate the username and password format
+func Validate(username string, password string) bool {
+
+	return true
+}
+
+// Handler - Handler to process the AWS API Gateway request to create a user
 func Handler(request Request) (Response, error) {
 
 	item := Item{
@@ -118,9 +132,11 @@ func Handler(request Request) (Response, error) {
 
 	DatabaseCreateUser(item)
 
+	key, _ := encoder.EncodeJWT(request.Username)
+
 	return Response{
 		Message: fmt.Sprintf("Hello %s, Your user is created", request.Username),
-		Key:     "null",
+		Key:     key,
 		Ok:      true,
 	}, nil
 }
